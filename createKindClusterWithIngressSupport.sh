4732659530bd33442e205cc400e58852affc9914
Tom Eyckmans <teyckmans@gmail.com>
2021-01-02 18:50:41 +0100

    update create kind cluster script.

--- createKindClusterWithIngressSupport.sh
+++ createKindClusterWithIngressSupport.sh
@@ -6,7 +6,13 @@
 # 4. Patch the nginx ingress controller as per Kind Ingress install instructions.
 #
 
-kind create cluster --config src/main/yaml/kind-ingress/kind-ingress-cluster.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.2/deploy/static/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.2/deploy/static/provider/baremetal/service-nodeport.yaml
-kubectl patch deployments -n ingress-nginx nginx-ingress-controller -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-ingress-controller","ports":[{"containerPort":80,"hostPort":80},{"containerPort":443,"hostPort":443}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+kind create cluster --config src/main/yaml/kind-ingress/kind-ingress-cluster.yaml --image=kindest/node:v1.20.0
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+sleep 10
+kubectl wait --namespace kube-system --for=condition=ready pod/etcd-kind-control-plane --timeout=90s
+kubectl wait --namespace kube-system --for=condition=ready pod/kube-apiserver-kind-control-plane --timeout=90s
+kubectl wait --namespace kube-system --for=condition=ready pod/kube-controller-manager-kind-control-plane --timeout=90s
+kubectl wait --namespace kube-system --for=condition=ready pod/kube-scheduler-kind-control-plane --timeout=90s
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=k8s-app=kube-dns --timeout=90s
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
+kubectl get pods --all-namespaces
