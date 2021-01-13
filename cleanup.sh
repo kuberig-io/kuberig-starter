39467d86b26ebf9f1ead47fa7a75d201b03d5332
Tom Eyckmans <teyckmans@gmail.com>
2021-01-13 20:44:30 +0100

    added cleanup script.

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+kubectl delete pod foo-app
+kubectl delete pod bar-app
+kubectl delete ingress example-ingress
+kubectl delete service foo-service
+kubectl delete service bar-service
+kubectl delete serviceaccount kuberig
+kubectl delete rolebinding kuberig-edit
